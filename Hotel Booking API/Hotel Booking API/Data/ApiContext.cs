e556f3ded05c5131bb79326df830e86cc43851c9
Joshua220174 <71901226+Joshua220174@users.noreply.github.com>
2025-01-27 00:54:04 +0000

    Basic Hotel Booking API

---
+++ Hotel Booking API/Hotel Booking API/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Hotel_Booking_API.Models;
+
+namespace Hotel_Booking_API.Data
+{
+    public class ApiContext : DbContext
+    {
+
+        public DbSet<HotelBooking> Bookings { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Hotel Booking API/Hotel Booking API/Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_Booking_API.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public string? ClientName { get; set; }
+    }
+}
